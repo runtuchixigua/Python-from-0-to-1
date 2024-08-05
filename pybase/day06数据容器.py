9cfe73c25e6440d127f5272ca1d0e05f0cbdec95
jiyucheng <1152779715@qq.com>
2024-08-05 14:18:31 +0800

    day05

---
+++ pybase/day06数据容器.py
@@ -0,0 +1,68 @@
+# 99乘法表
+# for i in range(1, 10):
+#     for j in range(1, i + 1):
+#         print(f'{i}*{j} = {i * j}', end=' ')
+#         j += 1
+#     print()
+#     i += 1
+
+
+# 多行字符串
+str1 = '''
+lsdalakdskfj
+lkasdjflkj
+alkdsjf
+'''
+
+# 特殊字符串定义
+# str2 = 'I\'m boy'
+# str3 = "I'm boy"
+# print(str2)
+# print(str3)
+
+
+# # 字符串索引下标
+# str4 = 'itniuma'
+# print(str4[0])
+# print(str4[1])
+# print(str4[2])
+# print(str4[3])
+# print(str4[4])
+# print(str4[5])
+# print(str4[6])
+# print('-' * 88)
+# print(len(str4))
+# print('-' * 88)
+# for i in str4:
+#     print(i)
+
+# 切片
+# str4 = 'itniuma'
+# str5 = '12345678'
+# # print(str4[1:5:2])
+# # print(str4[1::-1])
+# print(str5[::-1])
+# print(str5[-2::-1])
+# print(str5[1::-1])
+# print(str5[7:1:-1])
+# print('----------------------')
+# #下面交叉了这个打印不出来
+# print(str5[1:8:-1])
+# print(str5[8:1:1])
+
+
+# 子串查找和统计方法
+# str6 = ' 1 hello itniuma hello python'
+# print(str6.find('hello', 10))
+# print(str6.count('hello'))
+
+
+# 字符串修改方法
+# str = 'apple,banana,orange'
+# print(str.replace('apple', 'peach'))
+# print(str.split(','))
+# a = str.split(',')
+# b = ','.join(a)
+# print(b)
+
+
